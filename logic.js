89e037633e5b534dcc0552cdeacb7cb88250dca2
raj8888 <jadhavrj8877@gmail.com>
2022-09-13 20:22:35 +0530

    Updated prime number code

--- logic.js
+++ logic.js
@@ -1,7 +1,12 @@
 let number = 34;
-for (let i = 0; i < number; i++) {
+let count=0
+for (let i = 2; i < number; i++) {
   if (34 % i == 0) {
-    console.log("no");
+    count++;
   }
-  console.log("ues");
+}
+if(count==2){
+  console.log("Prime number")
+}else{
+  console.log("Not prime number")
 }
